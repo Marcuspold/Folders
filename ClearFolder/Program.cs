1907f99479d093ad77102bea9df42317dc1ee21c
Opilane TTHK <opilane@tthk.ee>
2020-11-18 15:10:39 +0200

    first commit

---
+++ ClearFolder/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace ClearFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Morning Soldier!");
+            DeleteAllFiles();
+            string RootPath = @"C:\Users\opilane\Samples";
+
+            DirectoryInfo rootDirectory = new DirectoryInfo(RootPath);
+            foreach(DirectoryInfo dir in rootDirectory.GetDirectories())
+            {
+                DeleteAllFolders(dir.FullName, true);
+            }
+
+           
+        }
+
+        //a function to delete all the files
+        
+        public static void DeleteAllFiles()
+        {
+            string RootPath = @"C:\Users\opilane\Samples";
+            DirectoryInfo Directory = new DirectoryInfo(RootPath);
+
+            foreach(FileInfo file in Directory.GetFiles())
+            {
+                file.Delete();
+            }
+
+
+        }
+        //a function to delete all the folders
+        public static void DeleteAllFolders(string DirectoryName, bool ifExists)
+        {
+            if (Directory.Exists(DirectoryName))
+            {
+                Directory.Delete(DirectoryName, true);
+            } else if (ifExists)
+            {
+                throw new SystemException("NO SUCH DIRECTORY TO DELETE FOOLISH FOOL");
+            }
+
+        }
+        
+            
+    }
+}
---
+++ CreateDirectory/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System;
+
+namespace CreateDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Good Morning Soldier!");
+            CreateFolderOne();
+            CreateFolderTwo();
+        }
+        public static void CreateFolderOne()
+        {
+            string RootPath = @"C:\Users\opilane\Samples";
+            string FolderName = "PlayerOne";
+            string FullFolderPath = Path.Combine(RootPath, FolderName);
+            Console.WriteLine(FullFolderPath);
+            if (Directory.Exists(FullFolderPath)) //check if folder exists
+            {
+                Console.WriteLine("Maybe You Should Use More Of Your Eyes?");
+            } else
+            {
+                Directory.CreateDirectory(FullFolderPath);
+            }
+        }
+
+
+        public static void CreateFolderTwo()
+        {
+            string RootPath = @"C:\Users\opilane\Samples";
+            string FolderName = "PlayerTwo";
+            string FullFolderPath = Path.Combine(RootPath, FolderName);
+            Console.WriteLine(FullFolderPath);
+            if (Directory.Exists(FullFolderPath)) //check if folder exists
+            {
+                Console.WriteLine("Maybe You Should Use More Of Your Eyes?");
+            } else
+            {
+                Directory.CreateDirectory(FullFolderPath);
+            }
+        }
+    }
+}
